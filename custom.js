2e7e4a994366b8f1d6ffe7f05ef8a7b7b9457680
SANKET SARKAR <47353707+sscodes@users.noreply.github.com>
2020-11-24 12:08:57 +0530

    Added Files.

---
+++ custom.js
@@ -0,0 +1,19 @@
+$(".slider-area").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+    autoplay: true,
+    });
+  $(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+  $(".center").slick({
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 3
+  });
